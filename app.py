fb1f7b7e8b773fd4af3fa577eb9df32a85fc2a51
Aigul <aigul.djon@gmail.com>
2023-10-30 17:47:01 +0000

    Added image upload view and volume in yml file.

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'images'
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['POST', 'GET'])
 def film_quote():
-    return "<strong>My mama always said life was like a box of chocolates. You never know what you're gonna get. - </strong> <i>Forrest Gump</i>"
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-if __name__ in "__main__":
-    app.run(host='0.0.0.0', port=5000)
+            return redirect(url_for('download_file', name=filename))
+    
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+        <input type=file name=file>
+        <input type=submit value=Upload>
+    </form>
+    '''
+        
+    
+    # return "<strong>My mama always said life was like a box of chocolates. You never know what you're gonna get. - </strong> <i>Forrest Gump</i>"
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
